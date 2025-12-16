c7ddbe3231a0b0ea15d8a2a1644190db1e506020
Jahnavi <gaurjahnavi84@gmail.com>
2025-12-16 10:08:22 +0530

    final code

--- src/components/SumCalculator.js
+++ src/components/SumCalculator.js
@@ -9,37 +9,27 @@ const SumCalculator = () => {
     setSum(numbers.reduce((acc, val) => acc + val, 0));
   }, [numbers]);
 
-
-
   const handleChange = (e) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value)) {
-      setInputValue(value);
+    const value = Number(e.target.value);
+
+    setInputValue(e.target.value);
+
+    if (!isNaN(value) && e.target.value !== "") {
       setNumbers((prev) => [...prev, value]);
-    } else {
-      setInputValue("");
     }
   };
 
   return (
     <div style={{ marginTop: "50px" }}>
-      <h1 style={{ fontWeight: "bold" }}>Sum Calculator</h1>
+      <h1>Sum Calculator</h1>
 
       <input
         type="number"
         value={inputValue}
         onChange={handleChange}
-        style={{
-          padding: "8px",
-          width: "200px",
-          fontSize: "18px",
-          marginTop: "20px",
-        }}
       />
 
-      <h3 style={{ marginTop: "20px", fontSize: "20px" }}>
-        Sum: {sum}
-      </h3>
+      <p>Sum: {sum}</p>
     </div>
   );
 };
